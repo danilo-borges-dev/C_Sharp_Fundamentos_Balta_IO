311036662634edf169cc9856f4fe5384594ff848
danilo borges <danilosilva2017@hotmail.com>
2023-01-03 07:17:44 -0300

    Iniciado o Projeto Stopwatch

--- StopWatch/StopWatch/Program.cs
+++ StopWatch/StopWatch/Program.cs
@@ -1,13 +1,40 @@
-﻿Start();
+﻿Menu();
 
-void Start()
+void Start(int time)
 {
-    int time = 1;
-
-    while (time <= 10)
+    int currentTime = 0;
+    while (currentTime <= time)
     {
         Console.Clear();
-        Console.WriteLine(time++);
+        Console.WriteLine(currentTime++);
         Thread.Sleep(1000);
     }
+    Console.WriteLine("Stopwatch finalizado.");
+}
+
+void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("nS - Segundos (10S - Significa 10 Segundos)");
+    Console.WriteLine("nM - Minutos (1M - Significa 1 Minuto)");
+    Console.WriteLine("nH - Horas (2H - Significa 2 Horas)");
+    Console.WriteLine("0 - Sair");
+    Console.WriteLine("Quanto tempo deseja contar? ");
+    string data = Console.ReadLine().ToUpper();
+    char type = char.Parse(data.Substring((data.Length - 1), 1));
+    int time = int.Parse(data.Substring(0, data.Length - 1));
+
+    int multiplier = 1;
+
+    if (type == 'M')
+    {
+        multiplier = 60;
+    }
+
+    if (type == '0')
+    {
+        Environment.Exit(0);
+    }
+
+    Start(time * multiplier);
 }
