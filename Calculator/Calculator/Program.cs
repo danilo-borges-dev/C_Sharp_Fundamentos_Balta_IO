4e81b9d684ccfd907357edd8b44589c811e04384
danilo borges <danilosilva2017@hotmail.com>
2023-01-03 06:26:58 -0300

    Fundamentos

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+//Soma();
+Subtracao();
+
+
+void Soma()
+{
+    Console.Write("Digite o primeiro número: ");
+    float n1 = float.Parse(Console.ReadLine());
+    Console.Write("Digite o segundo número: ");
+    float n2 = float.Parse(Console.ReadLine());
+
+    float result = n1 + n2;
+    Console.WriteLine($"Resultado da Soma é {result.ToString("F2", CultureInfo.InvariantCulture)}");
+}
+
+void Subtracao()
+{
+    Console.Write("Digite o primeiro número: ");
+    float n1 = float.Parse(Console.ReadLine());
+    Console.Write("Digite o segundo número: ");
+    float n2 = float.Parse(Console.ReadLine());
+
+    float result = n1 - n2;
+    Console.WriteLine($"Resultado da Subtração é {result.ToString("F2", CultureInfo.InvariantCulture)}");
+}
